7e5dbeecd3189cfc10b0ff3df5d28ca7915acb70
John Pogosyan <jpogosyan@cornerstoneist.com>
2013-08-22 22:39:48 -0700

    Changed config to production values
    Zendesk at cornerstoneist.zendesk.com
    
    Freshbooks at cornerstoneist.freshbooks.com

--- config/config.js
+++ config/config.js
@@ -2,39 +2,39 @@ module.exports = {
 	development: {
 		db: 'mongodb://heroku_app14220631:sa8fb17r1tvvijse1atvnrasbl@ds063177.mongolab.com:63177/heroku_app14220631',
 		zd: {
-			subdomain: 'cornerstoneistsandbox',
-		    username: 'harut.muradyan@simplytech.co',
-		    token: 'DOcSOMIZnuBXKFiTY9HwL0MyQ2TNGNhxLn2UBeTL'
+			subdomain: 'cornerstoneist',
+		    username: 'jpogosyan@cornerstoneist.com',
+		    token: 'lK5fuxstO87o2Hh7ZDW5pdzO56FcdNH6xj6DiDkm'
 		},
 
 		fb: {
-			host:'cornerstoneistsandbox.freshbooks.com',
-			token:'9936916e20a4f05e4a81889b235ccce9'
+			host:'cornerstoneist.freshbooks.com',
+			token:'10d8cfaddff7dc9e986d33f2b266b637'
 		}
 	},
 	production: {
 		//db: 'mongodb://localhost/zendesk',
 		db: 'mongodb://heroku_app14220631:sa8fb17r1tvvijse1atvnrasbl@ds063177.mongolab.com:63177/heroku_app14220631',
 		zd: {
-			subdomain: 'cornerstoneistsandbox',
-		    username: 'harut.muradyan@simplytech.co',
-		    token: 'DOcSOMIZnuBXKFiTY9HwL0MyQ2TNGNhxLn2UBeTL'
+			subdomain: 'cornerstoneist',
+		    username: 'jpogosyan@cornerstoneist.com',
+		    token: 'lK5fuxstO87o2Hh7ZDW5pdzO56FcdNH6xj6DiDkm'
 		},
 
 		fb: {
-			host:'cornerstoneistsandbox.freshbooks.com',
-			token:'9936916e20a4f05e4a81889b235ccce9'
+			host:'cornerstoneist.freshbooks.com',
+			token:'10d8cfaddff7dc9e986d33f2b266b637'
 		}
 	},
 	zd: {
-		 subdomain: 'cornerstoneistsandbox',
-	     username: 'harut.muradyan@simplytech.co',
-	     token: 'DOcSOMIZnuBXKFiTY9HwL0MyQ2TNGNhxLn2UBeTL'
+		 subdomain: 'cornerstoneist',
+	     username: 'jpogosyan@cornerstoneist.com',
+	     token: 'lK5fuxstO87o2Hh7ZDW5pdzO56FcdNH6xj6DiDkm'
 	},
 
 	fb: {
-		host:'cornerstoneistsandbox.freshbooks.com',
-		token:'9936916e20a4f05e4a81889b235ccce9'
+		host:'cornerstoneist.freshbooks.com',
+		token:'10d8cfaddff7dc9e986d33f2b266b637'
 	}
 
 }
